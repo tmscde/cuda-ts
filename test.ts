d1091f9254402820485f1c1237ada7a8582616a3
Tomas Kjellstrand <tomas.kjellstrand@gmail.com>
2019-05-21 07:27:01 +0200

    Removed redundant test file

--- test.ts
+++
@@ -1,8 +0,0 @@
-import * as cuda from "./src/cuda";
-
-cuda.compileCuToPtx(
-  `extern "C" __global__ void add(float* input1, float* input2, float* output) {
-    output[blockIdx.x] = input1[blockIdx.x] + input2[blockIdx.x];
-  }
-`,
-);
